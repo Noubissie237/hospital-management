71a7c51caa0c6c225364d955a31460ebbd387a93
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-01-10 08:51:08 +0100

    Gestion du personnel

---
+++ GestionPersonnel/GestionPersonnel/App/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+class Specialite(models.Model):
+    designation = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.designation
+
+class Medecin(AbstractUser):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, null=False, unique=True)
+    Specialite = models.ForeignKey(Specialite, null=True, on_delete=models.SET_NULL)
+    password = models.CharField(max_length=250, null=True)
+
+    def __str__(self):
+        return self.nom
+
+class Consultation(models.Model):
+    nom = models.CharField(max_length=50, null=True)
+    prenom = models.CharField(max_length=50, null=True)
+    email = models.EmailField(max_length=254, null=True)
+    age = models.IntegerField(null=True)
+    service = models.CharField(max_length=50)
+    sexe = models.CharField(max_length=50)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nom
+
+class Prescription(models.Model):
+    nom = models.CharField(max_length=100)
+    prenom = models.CharField(max_length=100, null=True)
+    age = models.IntegerField(null=True)
+    sexe = models.CharField(max_length=15)
+    email = models.EmailField(max_length=254, null=False)
+    antecedent = models.TextField(null=True)
+    prescription1 = models.CharField(max_length=150, null=True)
+    prescription2 = models.CharField(max_length=150, null=True)
+    prescription3 = models.CharField(max_length=150, null=True)
+    Token = models.CharField(max_length=10, null=True)
+
+    def __str__(self):
+        return self.nom
+    
+    
---
+++ GestionPersonnel/GestionPersonnel/App/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+router = DefaultRouter()
+router.register(r'prescriptions', PrescriptionViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('', home),
+    path('home/', home),
+    path('rendez-vous/', appointment),
+    path('consultations/', consultations),
+    path('prescription/', prescription),
+    path('file-d-attente/', file_d_attente),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('patient/', patient),
+]
