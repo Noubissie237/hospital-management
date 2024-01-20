0e7e6188dd84584c84eac4eec15b42060c97e56a
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2024-01-20 18:44:23 +0100

    New update

--- GestionRDV/GestionRDV/urls.py
+++ GestionRDV/GestionRDV/urls.py
@@ -3,7 +3,7 @@ from django.urls import include ,path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('client.urls')),
+    path('api/', include('App.urls')),
     path('api-auth/', include('rest_framework.urls'))
 ]
 
